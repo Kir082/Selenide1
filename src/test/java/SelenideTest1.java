5a665f2984690521bda87ae9a395f52da0a31b37
kir082 <kirillich082@gmail.com>
2025-05-11 17:16:56 +0300

    Добавил домашку по уроку 4. Selenide#1

---
+++ src/test/java/SelenideTest1.java
@@ -0,0 +1,38 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideTest1 {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void checkSoftAssertions(){
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-body").shouldHave(text("Soft assertions"));
+        $("#wiki-body").$(byText("Soft assertions")).click();
+        executeJavaScript("arguments[0].scrollIntoView(true);", $("[id='user-content-3-using-junit5-extend-test-class']"));
+        $(".markdown-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
+    }
+
+}
