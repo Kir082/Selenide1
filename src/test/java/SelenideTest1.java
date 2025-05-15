f463c3ebb05a3a5810cbb2f819a3b117763361ea
kir082 <kirillich082@gmail.com>
2025-05-15 09:40:09 +0300

    Добавил корректное имя для метода в BeforeAll

--- src/test/java/SelenideTest1.java
+++ src/test/java/SelenideTest1.java
@@ -2,14 +2,13 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideTest1 {
     @BeforeAll
-    static void beforeAll() {
+    static void setupConfig() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://github.com";
         Configuration.pageLoadStrategy = "eager";
